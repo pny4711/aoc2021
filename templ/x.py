85fc35bc8a7cb62637605173e16eb1320e1bb409
pny4711 <pny4711@nope.foo>
2021-12-11 10:43:56 +0100

    Add dummy print to template

--- templ/x.py
+++ templ/x.py
@@ -2,3 +2,7 @@ import os
 os.chdir(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))))
 
 lines = [line.strip() for line in open('x.input.ref')]
+
+a,b = 0, 0
+print(f"a: {a}")
+print(f"b: {b}")
