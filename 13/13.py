da8796e085b519f6de39ba743ceddb0394790b04
pny4711 <pny4711@nope.foo>
2021-12-13 13:30:53 +0100

    13 simplified some more

--- 13/13.py
+++ 13/13.py
@@ -1,9 +1,6 @@
-def is_foldx(x, fa, fn): return (fa == 'x' and x > fn)
-def is_foldy(y, fa, fn): return (fa == 'y' and y > fn)
-def is_fold(x, y, fa, fn): return is_foldx(x, fa, fn) or is_foldy(y, fa, fn)
 def fold(x, y, fa, fn):
-    return (fn - (x - fn) if is_foldx(x, fa, fn) else x, 
-            fn - (y - fn) if is_foldy(y, fa, fn) else y)
+    return (fn - (x - fn) if (fa == 'x' and x > fn) else x, 
+            fn - (y - fn) if (fa == 'y' and y > fn) else y)
 
 nr_dots = []
 lines,folds = open('13.input.txt').read().split('\n\n')
@@ -11,9 +8,7 @@ dots = set([(int(i) for i in l.split(',')) for l in lines.strip().split('\n')])
 
 for fl in folds.strip().split('\n'):
     fa,fn = fl.split()[2].split('=')
-    fn = int(fn)
-    dots = set([fold(x, y, fa, fn)  for x, y in dots 
-                if not is_fold(x, y, fa, fn) or fold(x, y, fa, fn) not in dots])
+    dots = set([fold(x, y, fa, int(fn))  for x, y in dots])
     nr_dots.append(len(dots))
 
 print(f"a: {nr_dots[0]}\n")
