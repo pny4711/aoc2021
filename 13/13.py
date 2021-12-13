9227f61fe1955eaafe68e9eca5142de760f0b5d0
pny4711 <pny4711@nope.foo>
2021-12-13 13:17:25 +0100

    13, simplified input and forst dots creation

--- 13/13.py
+++ 13/13.py
@@ -5,20 +5,16 @@ def fold(x, y, fa, fn):
     return (fn - (x - fn) if is_foldx(x, fa, fn) else x, 
             fn - (y - fn) if is_foldy(y, fa, fn) else y)
 
-nr_dots,dots = [], set()
-with open('13.input.txt') as f:
-    for line in f:
-        if line.strip() == '':
-            break
-        x,y = line.strip().split(',')
-        dots.add((int(x),int(y)))
+nr_dots = []
+lines,folds = open('13.input.txt').read().split('\n\n')
+dots = set([(int(i) for i in l.split(',')) for l in lines.strip().split('\n')])
 
-    for fl in [line.strip() for line in f]:
-        fa,fn = fl.split()[2].split('=')
-        fn = int(fn)
-        dots = set([fold(x, y, fa, fn)  for x, y in dots 
-                    if not is_fold(x, y, fa, fn) or fold(x, y, fa, fn) not in dots])
-        nr_dots.append(len(dots))
+for fl in folds.strip().split('\n'):
+    fa,fn = fl.split()[2].split('=')
+    fn = int(fn)
+    dots = set([fold(x, y, fa, fn)  for x, y in dots 
+                if not is_fold(x, y, fa, fn) or fold(x, y, fa, fn) not in dots])
+    nr_dots.append(len(dots))
 
 print(f"a: {nr_dots[0]}\n")
 for y in range(1+ max([y for _,y in dots])):
