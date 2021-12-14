7b36f11e1a2f62ea473ff7b2bf50125c0f460e1f
pny4711 <pny4711@nope.foo>
2021-12-14 11:12:03 +0100

    Fix off by one bug and extract dict merge

--- 14/14.py
+++ 14/14.py
@@ -4,35 +4,29 @@ for l in rulelines.strip().split('\n'):
     pair, insert = l.split('->')
     rules[pair.strip()] = insert.strip()
 
-line = line.strip()
-
 cache = {}
-cache_hits = 0
+
+def merge(d0, d, extra):
+    for ch, n in d:
+        if ch not in d0:
+            d0[ch] = 0
+        d0[ch] += n - 1 if ch == extra else n
 
 def expand_pair(times, pair):
-    if times == 0:
+    if times == 0: 
         return {pair[0]:2} if pair[0] ==pair[1] else {pair[0]:1, pair[1]:1}
-    if (times,pair) in cache:
+    if (times,pair) in cache: 
         return cache[(times,pair)]
     extra = rules[pair]
     data = {ch:v for ch,v in expand_pair(times-1, pair[0] + extra).items()}
-    for ch, v in expand_pair(times-1, extra + pair[1]).items():
-        if ch not in data:
-            data[ch] = v
-        else:
-            data[ch] += v - 1 if ch == extra else v
-        
+    merge(data, expand_pair(times-1, extra + pair[1]).items(), extra)        
     cache[(times,pair)] = data
     return data
 
 def expand(times, s):
-    res = {}
+    res = {line[0]:1}
     for i in range(len(s) - 1):
-        for k,n in expand_pair(times,line[i] + line[i+1]).items():
-            if k not in res:
-                res[k] = n
-            else:
-                res[k] += n - 1 if k == pair[0] else n
+        merge(res, expand_pair(times, line[i] + line[i + 1]).items(), line[i])
     return res
 
 vals1 = sorted([v for v in expand(10, line).values()])
